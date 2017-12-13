1bc34cb3bb7445d261bfb51bbe7afa6fdc401993
LCL <crforest0301@gmail.com>
2017-12-13 16:32:49 +0900

    基本ファイル追加

---
+++ ums/ums.sql
@@ -0,0 +1,20 @@
+create database ums character set utf8 collate utf8_general_ci;
+
+use ums;
+
+create table t_user(
+	user_id varchar(20) not null primary key, /*MEMO variable length QUESTION 20*/
+    name varchar(20) not null,
+    birth varchar(20) not null,
+    address varchar(20) not null,
+    pn varchar(20) not null,
+    pw varchar(20) not null,
+    status varchar(20) not null,
+    updated varchar(20) not null
+);
+
+insert into t_user values('000a', 'applea', '1991/1/1a', '東京都a', '031112322a', '1234a', 'ACTVa', '6/2/17 14:29a');
+/*insert into t_user values('a', 'a', 'a', 'a', 'a', 'a', 'a', 'a');*/
+insert into t_user values('b', 'b', 'b', 'b', 'b', 'b', 'b', 'b');
+
+
